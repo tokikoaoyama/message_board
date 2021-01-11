b74a7bc957dcd740deaffbff1b98668f303ed1bb
tokiko.aoyama <tokikoaoyama@gmail.com>
2021-01-11 02:05:21 +0000

    add db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+(1..100).each do |number|
+    Message.create(title: 'test title ' + number.to_s, content: 'test content ' + number.to_s)
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
