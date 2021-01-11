c883cd99821aae0f5f5655353225f8288eb7d8f6
tokiko.aoyama <tokikoaoyama@gmail.com>
2021-01-11 01:56:04 +0000

    add pagination

---
+++ db/migrate/20210111013323_add_titles_to_messages.rb
@@ -0,0 +1,5 @@
+class AddTitlesToMessages < ActiveRecord::Migration[5.2]
+  def change
+    add_column :messages, :title, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_061412) do
+ActiveRecord::Schema.define(version: 2021_01_11_013323) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "tasklists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
